debff674652b60f456add39cbc7a8d38a88d4b7f
Coffe <felixkot00@gmail.com>
2021-01-08 02:32:33 +0300

    Add site registration

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Actor, Category
+from .models import Actor, Category, Reviews, RatingStars, Rating, Genre, Movie, MovieShots
 # Register your models here.
-admin.site.register(Actor)
 admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStars)
+admin.site.register(Reviews)
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
 # Create your views here.
+#TODO lesson 6 (05:00 min)
